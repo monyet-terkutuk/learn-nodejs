193664410bb9dcb556a016563a32598cddd47c06
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-06-02 23:34:13 +0700

    - web server selesai

--- web_server/app.js
+++ web_server/app.js
@@ -1,1 +1,38 @@
 const http = require("http");
+const fs = require("fs");
+const port = 9000;
+
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    // Perubahan pada baris ini
+    if (err) {
+      res.writeHead(404);
+      res.write("Error: File not found! 404");
+    }
+    res.write(data);
+    res.end();
+  });
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  const url = req.url;
+
+  if (url === "/") {
+    renderHTML("./index.html", res);
+  }
+  if (url === "/about") {
+    renderHTML("./about.html", res);
+  }
+
+  if (url === "/contact") {
+    renderHTML("./contact.html", res);
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}..`);
+});
