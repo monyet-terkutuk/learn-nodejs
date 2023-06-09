5f0341d0ee1676e31948ecf58b8e800754948b9a
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-06-09 12:42:44 +0700

    - views engine

--- express/app.js
+++ express/app.js
@@ -1,60 +1,51 @@
-// const http = require("http");
-// const fs = require("fs");
-
 const express = require("express");
 app = express();
 const port = 9000;
+const expressLayouts = require("express-ejs-layouts");
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 app.get("/", (req, res) => {
-  // res.send("Hello Word!");
-  res.sendFile("./index.html", { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: "Siti Ayuni",
+      email: "siti@ayuni.com",
+      prodi: "Teknik Informatika - RPL",
+    },
+    {
+      nama: "Oryza Sativa",
+      email: "oryza@sativa.com",
+      prodi: "Teknik Informatika - SI",
+    },
+    {
+      nama: "Yang Yang",
+      email: "yang@yang.com",
+      prodi: "Teknik Informatika - SI",
+    },
+  ];
+  res.render("index", {
+    layout: "layouts/main",
+    nama: "Gabriel Yonathan",
+    title: "Home Page",
+    mahasiswa,
+  });
 });
 
 app.get("/about", (req, res) => {
-  // res.send("ini about");
-  res.sendFile("./about.html", { root: __dirname });
+  res.render("about", {
+    layout: "layouts/main",
+    title: "About Page",
+  });
 });
 
 app.get("/contact", (req, res) => {
-  // res.send("ini contact");
-  res.sendFile("./contact.html", { root: __dirname });
+  res.render("contact", {
+    layout: "layouts/main",
+    title: "Contact Page",
+  });
 });
 
 app.listen(port, () => {
   console.log(`App listening in port http://localhost:${port}`);
 });
-
-// const renderHTML = (path, res) => {
-//   fs.readFile(path, (err, data) => {
-//     // Perubahan pada baris ini
-//     if (err) {
-//       res.writeHead(404);
-//       res.write("Error: File not found! 404");
-//     }
-//     res.write(data);
-//     res.end();
-//   });
-// };
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, {
-//     "Content-Type": "text/html",
-//   });
-
-//   const url = req.url;
-
-//   if (url === "/") {
-//     renderHTML("./index.html", res);
-//   }
-//   if (url === "/about") {
-//     renderHTML("./about.html", res);
-//   }
-
-//   if (url === "/contact") {
-//     renderHTML("./contact.html", res);
-//   }
-// });
-
-// server.listen(port, () => {
-//   console.log(`Server is listening on port ${port}..`);
-// });
