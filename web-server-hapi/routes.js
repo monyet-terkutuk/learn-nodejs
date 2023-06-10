24199b2ee892776264484293c8b02678fa403207
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-06-11 00:16:46 +0700

    - beres

---
+++ web-server-hapi/routes.js
@@ -0,0 +1,54 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if (lang === "id") {
+        return `Hi, ${name}`;
+      }
+
+      return `Hello, ${name}!`;
+    },
+  },
+  //   curl -X GET http://localhost:5000/hello/dicoding
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
+];
+
+module.exports = routes;
--- web_service/next_server.js
+++ web_service/next_server.js
@@ -2,26 +2,39 @@ const http = require("http");
 
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
+  response.setHeader("X-Powered-By", "NodeJS");
   response.statusCode = 200;
 
-  const { method } = request;
+  const { method, url } = request;
 
-  if (method === "GET") {
-    response.end("Hello get!");
-  }
-
-  if (method === "POST") {
-    let body = [];
-
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}!</h1>`);
-    });
+  if (url === "/") {
+    if (method === "GET") {
+      response.end("<h1>Ini adalah Home Page</h1>");
+    } else {
+      response.end(
+        `<h1>Halaman tidak bisa di akses dengan method ${method} request!</h1>`
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end("<h1>Halo! Ini adalah halaman about.</h1>");
+    } else if (method === "POST") {
+      let body = [];
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+      request.on("end", () => {
+        body = Buffer.concat(body.toString);
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Halo ${name}, ini adalah halaman about!</h1>`);
+      });
+    } else {
+      esponse.end(
+        `<h1>Halaman tidak bisa di akses dengan method ${method} request!</h1>`
+      );
+    }
+  } else {
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
 
